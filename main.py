adc3d6106f77f5e6270f013572e14a339fa2ea4c
jhwilbert@gmail.com <joaow@hallocal.local>
2013-01-15 00:35:22 +0000

    ready for LIVE push really

--- main.py
+++ main.py
@@ -166,9 +166,8 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write(template.render(path, template_values))
         
 
-app = webapp2.WSGIApplication([('/', Redirect),
+app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/r', Redirect),
-                               ('/main', MainHandler),
                                ('/admin', Admin),
                                ('/a/add', Add),
                                ('/a/remove', Remove),
